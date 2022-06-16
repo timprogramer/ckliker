9381b3e3d1b5729169d38322c9a6c66966321ef9
Tim GF63 <tim@shatzir.com>
2022-06-16 19:58:43 +0900

    616

--- main.py
+++ main.py
@@ -1,18 +1,49 @@
 import wrap,time,random
 chet=0
 balance=6
-wrap.world.create_world(920,480)
-wrap.world.set_back_color(255,255,255)
+xspawn=60
+yspawn=90
+wrap.world.create_world(847,480)
 wrap.add_sprite_dir("mysprite")
-dog=wrap.sprite.add("g.dog",60,90,"g.dog")
+wrap.world.set_back_image('C:/Users/Tim GF63/Downloads/map2.jpg')
+rbp=wrap.sprite.add("player",424,415,"player")
+dog=wrap.sprite.add("g.dog",xspawn,yspawn,"g.dog")
+pet=dog
 @wrap.on_mouse_down(wrap.BUTTON_LEFT)
 def cklic():
     global chet
     chet=chet+balance
     print(chet)
+
+@wrap.on_key_always(wrap.K_RIGHT)
+def right():
+    global rbp
+    wrap.sprite.move(rbp,10,0)
+
+@wrap.on_key_always(wrap.K_LEFT)
+def left():
+    global rbp
+    wrap.sprite.move(rbp,-10,0)
+
+@wrap.on_key_always(wrap.K_UP)
+def up():
+    global rbp
+    wrap.sprite.move(rbp,0,-10)
+
+@wrap.on_key_always(wrap.K_DOWN)
+def down():
+    global rbp
+    wrap.sprite.move(rbp,0,10)
+
+
 @wrap.always()
 def always(pos_x,pos_y):
-    global chet
+    global chet,balance
+    xp=wrap.sprite.get_x(rbp)
+    yp=wrap.sprite.get_y(rbp)
+    wrap.sprite.move_to(pet,xp-60,yp)
     if chet >=1000 and wrap.sprite.is_collide_point(dog,pos_x,pos_y)  :
-        wrap.sprite.remove(dog)
-        wrap.sprite.add()
+        wrap.sprite.hide(dog)
+        wrap.sprite.add("g.cow",xspawn,yspawn,"g.cow")
+        balance=21
+        chet=chet-1000
