fb73d7ce0997e4180a91f2cc4e0cf464fff2fa73
Tim GF63 <tim@shatzir.com>
2022-06-17 17:47:55 +0900

    617

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ wrap.add_sprite_dir("mysprite")
 wrap.world.set_back_image('C:/Users/Tim GF63/Downloads/map2.jpg')
 rbp=wrap.sprite.add("player",424,415,"player")
 dog=wrap.sprite.add("g.dog",xspawn,yspawn,"g.dog")
+goldcow = wrap.sprite.add("g.cow", xspawn, yspawn, "g.cow")
+wrap.sprite.hide(goldcow)
 pet=dog
 @wrap.on_mouse_down(wrap.BUTTON_LEFT)
 def cklic():
@@ -38,12 +40,19 @@ def down():
 
 @wrap.always()
 def always(pos_x,pos_y):
-    global chet,balance
+    global chet,balance,pet
     xp=wrap.sprite.get_x(rbp)
     yp=wrap.sprite.get_y(rbp)
     wrap.sprite.move_to(pet,xp-60,yp)
-    if chet >=1000 and wrap.sprite.is_collide_point(dog,pos_x,pos_y)  :
-        wrap.sprite.hide(dog)
-        wrap.sprite.add("g.cow",xspawn,yspawn,"g.cow")
+    if chet >=1000 and wrap.sprite.is_collide_point(pet,pos_x,pos_y) and pet==dog:
+        wrap.sprite.remove(pet)
+        wrap.sprite.show(goldcow)
+        pet=goldcow
         balance=21
         chet=chet-1000
+    if chet >=4500 and wrap.sprite.is_collide_point(pet,pos_x,pos_y) and pet==goldcow:
+        wrap.sprite.remove(pet)
+        goldcorg=wrap.sprite.add("g.corg",xspawn,yspawn,"g1.crgie")
+        pet=goldcorg
+        balance=75
+        chet=chet-4500
